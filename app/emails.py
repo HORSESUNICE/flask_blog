384dbc1b8c4befaf5192e57d014c9e374784c669
HORSESUNICE <mathmyb@gmail.com>
2017-06-17 22:40:49 +0800

    modify send_email

--- app/emails.py
+++ app/emails.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from threading import Thread
 from flask import current_app
+from flask import render_template
 
 from . import mail
 
@@ -18,3 +19,12 @@ def send_email(subject, template, *to, **kwargs):
     thr.start()
     return thr
     # send_async_email(app, msg)
+
+def send_confirm_email(confirm_type, *to, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + confirm_type,
+                  sender=app.config['MAIL_SENDER'], recipients=[*to])
+    msg.body = render_template('auth/'+ confirm_type +'_confirm.txt', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr

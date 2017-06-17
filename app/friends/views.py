d9d5fa0dd44cc7f2b2a864b6ed8cd32aa1060681
HORSESUNICE <mathmyb@gmail.com>
2017-06-17 22:42:01 +0800

    add permission_confirmed

--- app/friends/views.py
+++ app/friends/views.py
@@ -1,17 +1,21 @@
 from flask import render_template, session, redirect, url_for, abort
+from flask_login import login_required
 
 from datetime import datetime
 
 from . import fd
-from ..main.forms import NameForm
-from ..models import Friend
+from ..main.forms import LoginForm
+from ..models import User, Permission
 from ..emails import send_email
+from ..decorators import permission_required
 
 @fd.route('/', methods=['GET', 'POST'])
+@permission_required(Permission.FRIEND)
+@login_required
 def friends():
-    form = NameForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        friend = Friend.query.filter_by(name=form.name.data).first()
+        friend = User.query.filter_by(name=form.name.data).first()
         if friend:
             session['name'] = friend.name
             session['realname'] = friend.realname
@@ -22,10 +26,12 @@ def friends():
     return render_template('friends.html', form=form)
 
 @fd.route('/<name>')
+@permission_required(Permission.FRIEND)
+@login_required
 def friend(name):
     pre = session.get('pre', None)
     if name == pre:
-        friend = Friend.query.filter_by(name=name).first()
+        friend = User.query.filter_by(name=name).first()
         accounts = friend.gameaccounts.all()
         # begin = datetime.now()
         # send_email(' Welcome', 'welcome', 'yourqq@qq.com', 'your163@163.com', name=session['realname'], accounts=accounts)

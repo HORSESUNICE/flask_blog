92b26ccf9e8c9bc1725be00958e9f31ccf08f611
HORSESUNICE <mathmyb@gmail.com>
2017-06-17 22:37:06 +0800

    add flask_login and auth

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,19 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
+    # __name__='app'
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
@@ -19,11 +24,15 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .friends import fd as friends_blueprint
     app.register_blueprint(friends_blueprint, url_prefix='/friends')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
